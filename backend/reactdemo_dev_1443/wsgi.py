3a7b587e988d5d92b3a26386b2d9303afd758c58
Crowdbotics <team@crowdbotics.com>
2020-01-08 08:26:02 +0000

    Update models

--- backend/reactdemo_dev_1443/wsgi.py
+++ backend/reactdemo_dev_1443/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reactdemo_dev_1443.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reactdemo_dev_1443.settings")
 
 application = get_wsgi_application()
